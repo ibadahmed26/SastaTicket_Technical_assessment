1cfcb1069a634369303b81cf735324c6cb1d89c8
ebadahmed <ibadahmed26@gmail.com>
2022-02-18 01:50:36 +0500

    feat:configure user model and jwt authentication

--- resume_collector_webapp/resumecollector/views.py
+++ resume_collector_webapp/resumecollector/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import UserSerializer
 
-# Create your views here.
+
+class RegisterUser(APIView):
+    def post(self, request):
+        payload = request.data
+        serialized = UserSerializer(data=payload)
+        serialized.is_valid(raise_exception=True)
+        serialized.save()
+        return Response(
+            {'success': True,
+             'msg': 'User Signed up'},
+            status=status.HTTP_201_CREATED
+        )
